bd01f23fa97128a4e997fc5bd40b11f9a3a4e96c
“TechWithAshish” <Ashish9758592845@gmail.com>
2026-03-23 19:59:05 +0530

    chnaged dependency

--- src/main/java/gcfv2/HelloHttpFunction.java
+++ src/main/java/gcfv2/HelloHttpFunction.java
@@ -87,6 +87,7 @@ public class HelloHttpFunction implements HttpFunction {
       return true;
     } catch (Exception e) {
       // Validation failed (token expired, wrong signature, etc.)
+      e.printStackTrace();
       response.setStatusCode(401);
       System.out.println("Token Validation failed:----"+e.getMessage());
       response.getWriter().write("Token validation failed: " + e.getMessage());
